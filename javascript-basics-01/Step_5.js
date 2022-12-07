5d2f9c886a0b2158a81a213708631a30e283994c
Emile Kanaan <emilekanaan99@gmail.com>
2022-12-07 16:29:34 +0200

    Completed Step_5 in javascript-basics-01 exercise

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,9 @@
+let first = prompt("What's your first number?");
+let second = prompt("What's your second number?");
+
+document.getElementById("first_number").value = first;
+document.getElementById("second_number").value = second
+
+document.getElementById("validate").onclick = function () {
+    alert("The multiplication of " + first + " and " + second + " is = " + (first*second));
+}
