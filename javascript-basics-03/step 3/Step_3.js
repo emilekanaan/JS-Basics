814e4cbdc479895b5b93eae40f75525f914b7beb
Emile Kanaan <emilekanaan99@gmail.com>
2022-12-08 12:54:12 +0200

    Completed step 3 in javascript-basics-03 exercise

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,11 @@
+document.querySelector(".green").onclick = function () {
+    document.querySelector("#text").style.color = "green";
+}
+
+document.querySelector(".red").onclick = function () {
+    document.querySelector("#text").style.color = "red";
+}
+
+document.querySelector(".blue").onclick = function () {
+    document.querySelector("#text").style.color = "blue";
+}
