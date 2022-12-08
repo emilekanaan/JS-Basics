7e45cd15c50e447795602c78d4b7777ea54239d9
Emile Kanaan <emilekanaan99@gmail.com>
2022-12-08 16:45:23 +0200

    Completed from the beginning to the createLanguagesArray in javascript-basics-4-tdd exercise

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -1,63 +1,66 @@
 
 const stringSize = (text) => {
-
+    return text.length;
 }
 const replaceCharacterE = (text) => {
-
+    return text.replace("e", " ")
 }
 const concatString = (text1, text2) => {
-
+    return text1.concat(text2);
 }
 const showChar5 = (text) => {
-
+    return text[4];
 }
 const showChar9 = (text) => {
-
+    return text.slice(0, 9);
 }
 const toCapitals = (text) => {
-
+    return text.toUpperCase();
 }
 const toLowerCase = (text) => {
-
+    return text.toLowerCase();
 }
 const removeSpaces = (text) => {
-
+    return text.trim();
 }
 const IsString = (text) => {
-
+    if (typeof text === "string") {
+        return true;
+    }
 }
 
 const getExtension = (text) => {
-
+    return text.split(".")[1];
 }
 const countSpaces = (text) => {
-
+    return text.split(" ").length -1;
 }
 const InverseString = (text) => {
-
+    return text.split('').reverse().join('');
 }
 
 const power = (x, y) => {
-
+    return Math.pow(x,y);
 }
 const absoluteValue = (num) => {
-
+    return Math.abs(num);
 }
 const absoluteValueArray = (array) => {
-
+    return array.map(Math.abs);
 }
 const circleSurface = (radius) => {
-
+    return Math.ceil(Math.PI * Math.pow(radius,2)); 
 }
 const hypothenuse = (ab, ac) => {
-
+    return Math.hypot(ab, ac);
 }
 const BMI = (weight, height) => {
-
+    return Number((weight /Math.pow(height,2)).toFixed(2));
 }
 
 const createLanguagesArray = () => {
-
+    const array = ["Html","CSS","Java","PHP"];
+    return array;
 }
 
 const createNumbersArray = () => {
